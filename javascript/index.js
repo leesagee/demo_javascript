789c7a3134659d7ccffd129989e57bfbca697eb9
leesagee <lisag100@yahoo.com>
2021-10-04 13:58:12 -0700

    updating loctation of html files

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,5 @@
-document.querySelector("root")
-
 // JS button functionality
 function startJS() {
     // Opens new page
-    location.href = "/html_pages/questions.html"   
+    location.href = "/questions.html"   
 }
